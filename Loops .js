b90c27505d7e5d99db89a7fa693388b1bc7289ee
경모 <86238164+ProJKM@users.noreply.github.com>
2021-12-29 03:52:27 +0900

    Create Loops .js

---
+++ Loops .js
@@ -0,0 +1,16 @@
+var name = "ken";
+
+for (var i = 2; i > -1; i--) {
+  console.log(name[i]);
+}
+
+// 역순으로 한 글자씩 출력하는 반복문을 작성해보세요.
+
+var longText = "0i1a2m3k4e5n";
+
+for (var i = 0; i < longText.length; i++) {
+  if (i%2 != 1)
+  console.log(longText[i]);
+}
+
+// 홀수번째 글자만 출력하는 코드를 작성해보세요.
